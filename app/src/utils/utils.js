103a181582ca1dd87ecfd83c1808086ac621c0e1
Rodrigo Silva <rodrigojsdeveloper@gmail.com>
2024-07-30 11:43:04 -0300

    feat: add regex to change filename with space to dash

--- app/src/utils/utils.js
+++ app/src/utils/utils.js
@@ -1,16 +1,13 @@
-const formatStrToNoSpecialChars = (str) => {
-  return str.replace(/[^\w\s.]/gi, '');
+export const formatStrToNoSpecialChars = (str) => {
+  return str.replace(/[^\w\s.]/gi, '').replace(/\s/g, '-');
 };
 
-  const truncateFileName = (filename, maxLength = 25) => {
-    if (filename.length <= maxLength) return filename;
-  
-    const ext = filename.slice(filename.lastIndexOf('.'));
-    const nameWithoutExt = filename.slice(0, filename.lastIndexOf('.'));
-    const truncatedName = nameWithoutExt.slice(0, maxLength - ext.length);
-  
-    return truncatedName + ext;
-  };
-  
-  export { formatStrToNoSpecialChars, truncateFileName };
-  
+export const truncateFileName = (filename, maxLength = 25) => {
+  if (filename.length <= maxLength) return filename;
+
+  const ext = filename.slice(filename.lastIndexOf('.'));
+  const nameWithoutExt = filename.slice(0, filename.lastIndexOf('.'));
+  const truncatedName = nameWithoutExt.slice(0, maxLength - ext.length);
+
+  return truncatedName + ext;
+};
